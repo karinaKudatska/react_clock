76cef30d0f8e966e17143de9ea72d2e2d007bcb7
karinaKudatska <karina.kudatska@gmail.com>
2020-09-03 15:31:18 +0300

    Updates

---
+++ precache-manifest.1e17982ab10554ff466fb3cc9b9efcd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbf76cb6f90afadfd55be3d2a990e47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f039d27c7cef364f67f",
+    "url": "./static/css/main.6bb96bc0.chunk.css"
+  },
+  {
+    "revision": "8daccb04a7f88300915b",
+    "url": "./static/js/2.754e2893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.754e2893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f039d27c7cef364f67f",
+    "url": "./static/js/main.23b157fc.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  },
+  {
+    "revision": "39df0c5c2e90dd904c60bffc001e9249",
+    "url": "./static/media/computer.39df0c5c.png"
+  }
+]);
